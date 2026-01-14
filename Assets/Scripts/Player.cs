3dac4714145b5f907871df37be3d3f280b23d065
kaybern1512 <vovandong1512@gmail.com>
2026-01-14 19:19:04 +0700

    first time

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    Rigidbody2D rb;
+    Vector2 moveInput;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // đọc input từ bàn phím
+        moveInput.x = Keyboard.current.aKey.isPressed ? -1 :
+                      Keyboard.current.dKey.isPressed ? 1 : 0;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+    }
+}
