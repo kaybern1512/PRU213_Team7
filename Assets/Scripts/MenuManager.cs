7e0397ff2ae8c144d8512207e30925947e7aaea8
NgocViet1 <viettruongngoc@gmail.com>
2026-01-21 21:37:07 +0700

    fix menu and change skin

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject defaultRabbitPrefab;
+
+    public void ChangeSkin()
+    {
+        SceneManager.LoadScene("ChangeCharacter");
+    }
+
+    public void PlayGame()
+    {
+        if (CharacterSelect.selectedCharacter == null)
+        {
+            CharacterSelect.selectedCharacter = defaultRabbitPrefab;
+        }
+
+        SceneManager.LoadScene("GameScene");
+    }
+}
