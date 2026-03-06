de47c2dc989322e2d45ceb52b625f757cf578e08
ThaiTan <ttthptnt2000@gmail.com>
2026-03-06 11:54:19 +0700

    feat: background image

---
+++ Assets/Scripts/BackgroundScaler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        // Reset scale về 1
+        transform.localScale = Vector3.one;
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+        // Tính toán kích thước màn hình trong không gian thế giới
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        // Ép scale của ảnh khớp với kích thước màn hình
+        transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
+    }
+}
---
+++ Assets/Scripts/StaticBackground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class StaticBackground : MonoBehaviour
+{
+    private Transform camTransform;
+
+    void Start()
+    {
+        // Tự động tìm Camera chính
+        camTransform = Camera.main.transform;
+    }
+
+    void LateUpdate()
+    {
+        // Ép vị trí của Background luôn trùng với Camera
+        // Giữ nguyên trục Z ban đầu của ảnh
+        transform.position = new Vector3(camTransform.position.x, camTransform.position.y, transform.position.z);
+    }
+}
